b0e92ec71725fac20f709ccf035b89bd9ba0595d
aidosgal <agalimzhan928@gmail.com>
2025-03-16 02:17:10 +0500

    feat(news): add news section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Client from "./components/Client.tsx"
 import Footer from "./components/Footer.tsx"
 import Layout from "./components/Layout.tsx"
 import Main from "./components/Main.tsx"
+import News from "./components/News.tsx"
 import Project from "./components/Project.tsx"
 import Service from "./components/Service.tsx"
 
@@ -12,6 +13,7 @@ function App() {
       <Service/>
       <Client/>
       <Project/>
+      <News />
       <Footer/>
     </Layout>
   )
